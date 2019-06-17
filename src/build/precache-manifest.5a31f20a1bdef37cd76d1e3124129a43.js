155e7ba81a769b5a4af9eb869426da7750ceaa4a
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-06-17 13:27:42 +0200

    Make backend serve frontend

---
+++ src/build/precache-manifest.5a31f20a1bdef37cd76d1e3124129a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe4681cfd8163b2909aafacf880a552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7577825ad431759cfab",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "616b99912f4e67e5f5d2",
+    "url": "/static/js/2.1e6f9288.chunk.js"
+  },
+  {
+    "revision": "c7577825ad431759cfab",
+    "url": "/static/js/main.ea7a72d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
